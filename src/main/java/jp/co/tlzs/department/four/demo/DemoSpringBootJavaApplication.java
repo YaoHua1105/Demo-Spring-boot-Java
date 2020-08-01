07e82dda3ed88c87c350a6f31f6d2bf4821ff9b7
chen09 <chen09@gmail.com>
2020-08-01 22:19:07 +0900

    init

---
+++ src/main/java/jp/co/tlzs/department/four/demo/DemoSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package jp.co.tlzs.department.four.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootJavaApplication.class, args);
+	}
+
+}
