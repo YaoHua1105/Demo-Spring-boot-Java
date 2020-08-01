ce070c89266979615c51dd3848601a8a14355170
yaohua <kakeisyou1105@yahoo.co.jp>
2020-08-02 00:19:15 +0900

    no message

---
+++ src/main/java/jp/co/tlzs/department/four/demo/TestBean.java
@@ -0,0 +1,13 @@
+package jp.co.tlzs.department.four.demo;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
