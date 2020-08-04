399884d0d01864dd1fff6decb876d72d1681f464
yaohua <kakeisyou1105@yahoo.co.jp>
2020-08-04 23:04:45 +0900

    no message

--- src/main/java/jp/co/tlzs/department/four/demo/TestController.java
+++ src/main/java/jp/co/tlzs/department/four/demo/TestController.java
@@ -1,8 +1,9 @@
 package jp.co.tlzs.department.four.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 public class TestController {
     @GetMapping("/test")
